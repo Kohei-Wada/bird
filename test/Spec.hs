c8411d6eb92edaad448c4bf6ce31c5563116433e
Kohei Wada <program3152019@gmail.com>
2023-03-25 03:05:41 +0900

    added packages and some sample code

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,25 @@
+import System.IO.Silently
+import Control.Monad
+import Test.HUnit
+
+import BirdTest (birdTests)
+
+testHello :: Test
+testHello = TestCase $ do
+    let expectedOutput = concat $ replicate 20 "Hello\n"
+    actualOutput <- capture_ $ putStrLn "Hello" >> replicateM_ 19 (putStrLn "Hello")
+    assertEqual "Output does not match" expectedOutput actualOutput
 
 
+test2 :: Test
+test2 = TestCase $ do 
+    print "hello"
 
-main :: IO ()
-main = do 
+tests :: Test
+tests = TestList
+    [ TestLabel "testHello" testHello
+    , birdTests
+    ]
     
+main :: IO Counts
+main = runTestTT tests
