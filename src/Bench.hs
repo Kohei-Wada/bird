e094e269f592fd5a80cce54ad0258d5bd15aa7f7
Kohei Wada <program3152019@gmail.com>
2023-03-27 04:26:40 +0900

    added bench marks

--- src/Bench.hs
+++ src/Bench.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Bench where
 
 import System.CPUTime
@@ -10,14 +11,23 @@ import Score
 import Game
 import Actor
 
+import Control.Monad
+
 
 bench :: IO a -> IO () 
 bench m = do 
-    start <- getCPUTime
-    m
-    end <- getCPUTime
-    print $ "it takes " <> show (end - start) <> "[ps]"
+    results <- forM [1..10] $ \i -> do 
+        start <- getCPUTime
+        m
+        end <- getCPUTime
+        pure (end - start)
 
+    print results
+    print $ "it takes " <> show (average results) <> "[ps]"
+
+    where
+        average :: [Integer] -> Integer
+        average ns = sum ns `div` fromIntegral (length ns)
 
 benchMain :: IO () 
 benchMain = do 
@@ -27,48 +37,55 @@ benchMain = do
     benchUpdatePipes
     benchUpdateGameState
     benchUpdateGameObjects
+    benchResetGame
     benchUpdateGame
 
 benchUpdateSky :: IO () 
 benchUpdateSky = do 
     print "sky"
-    s <- initialize :: IO Sky
+    !s <- initialize :: IO Sky
     bench $ pure $ update s
 
 benchUpdateBird :: IO ()
 benchUpdateBird = do 
     print "bird"
-    b <- initialize :: IO Bird
+    !b <- initialize :: IO Bird
     bench $ pure $ update b
 
 benchUpdateGround :: IO ()
 benchUpdateGround = do 
     print "ground"
-    g <- initialize :: IO Ground
+    !g <- initialize :: IO Ground
     bench $ pure $ update g
 
 benchUpdatePipes :: IO () 
 benchUpdatePipes = do 
     print "pipes"
-    p <- initialize :: IO Pipes
+    !p <- initialize :: IO Pipes
     bench $ pure $ update p
 
 benchUpdateGameState :: IO () 
 benchUpdateGameState = do 
     print "game state"
-    g <- gameInit
+    !g <- gameInit
     bench $ pure $ updateGameState g
 
 benchUpdateGameObjects :: IO () 
 benchUpdateGameObjects = do 
     print "game objects"
-    g <- gameInit
+    !g <- gameInit
     bench $ pure $ updateGameObjects g
 
 benchUpdateGame :: IO () 
 benchUpdateGame = do 
     print "game"
-    g <- gameInit
+    !g <- gameInit
     bench $ updateGame 0 g
+
+benchResetGame :: IO () 
+benchResetGame = do 
+    print "game reset"
+    !g <- gameInit
+    bench $ gameReset g
 
 
